edff0621272544b037b639980cd788e98171083c
eibisidi <1029639542@qq.com>
2019-12-27 16:53:16 +0800

    删除无用代码，变量重命名

--- libHook/libHook.cpp
+++ libHook/libHook.cpp
@@ -3,20 +3,16 @@
 
 #include "stdafx.h"
 #include "libHook.h"
-#include <fstream>
 
 extern HMODULE hookModule;
-HHOOK HookPaste;	//Paste
-HWND HookerTid;
-std::ofstream outFile;
-
+HHOOK	HookPaste;		//Paste
+HWND	cpcpAppWnd;		//cpcpAppWnd Handle
 
 LRESULT CALLBACK KeyBoard_LL(int nCode,WPARAM wParam,LPARAM lParam)
 {
 	if (nCode < 0)  // do not process message 
 		return CallNextHookEx(HookPaste, nCode, wParam, lParam); 
 
-	BOOL ret;
 	KBDLLHOOKSTRUCT *  key = (KBDLLHOOKSTRUCT *)lParam;
 	switch (wParam)
 	{
@@ -24,24 +20,17 @@ LRESULT CALLBACK KeyBoard_LL(int nCode,WPARAM wParam,LPARAM lParam)
 		if (0x56 == key->vkCode
 			&& (GetAsyncKeyState(VK_LWIN) & 0x8000))
 #if 0
+			//Shift + Ctrl + V Alternative
 			&& (GetAsyncKeyState(VK_SHIFT) & 0x8000)
 			&& (GetAsyncKeyState(VK_CONTROL) & 0x8000))
 #endif
 		{//V key pressed while Shift & Ctrl key down
 			HWND hForeWnd = GetForegroundWindow();
-			ret = PostMessage(HookerTid, WM_HOOKER_PASTE, (WPARAM)hForeWnd, 0);
-			outFile << "Win + V" << ret << std::endl;
+			PostMessage(cpcpAppWnd, WM_HOOKER_PASTE, (WPARAM)hForeWnd, 0);
 			return 1;
 		}
 		break;
 	case WM_KEYUP:
-
-
-		if (0x56 == key->vkCode
-			&& (GetAsyncKeyState(VK_LWIN) & 0x8000))
-		{
-			//todo
-		}
 		break;
 	}
 
@@ -51,8 +40,7 @@ LRESULT CALLBACK KeyBoard_LL(int nCode,WPARAM wParam,LPARAM lParam)
 void WINAPI InstallHook(HWND hWnd)
 {
 	HookPaste=(HHOOK)SetWindowsHookEx(WH_KEYBOARD_LL,(HOOKPROC)KeyBoard_LL, hookModule,0);
-	HookerTid = hWnd;
-	outFile.open("save.log");
+	cpcpAppWnd = hWnd;
 }
 
 void WINAPI UninstallHook()
